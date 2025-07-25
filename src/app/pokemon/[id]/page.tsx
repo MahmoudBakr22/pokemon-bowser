449225de29bdc04afbf8afde138dd668bc0b20dc
midobakr <63980037+midobakr@users.noreply.github.com>
2025-07-25 15:27:26 +0300

    add SEO metaData  for every dragon

--- src/app/pokemon/[id]/page.tsx
+++ src/app/pokemon/[id]/page.tsx
@@ -1,3 +1,5 @@
+import { Metadata } from "next";
+
 import { getPokemonDetails } from "@/api/pokemon";
 import PokemonDetails from "@/components/pages/pokemonDetails";
 
@@ -5,4 +7,14 @@ export default async function PokemonPage({ params }: { params: Promise<{ id: st
   const { id: pokemonId } = await params;
   const pokemonDetails = await getPokemonDetails({ id: pokemonId });
   return <PokemonDetails details={pokemonDetails} />;
+}
+
+export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
+  const { id: pokemonId } = await params;
+  const pokemonDetails = await getPokemonDetails({ id: pokemonId });
+
+  return {
+    title: `${pokemonDetails.name} Dragon`,
+    description: `See Information about ${pokemonDetails.name} Dragon`,
+  };
 }
